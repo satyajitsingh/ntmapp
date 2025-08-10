a6e6a271ee3f4c294517f6d70949a5771eabe452
Satyajit Singh <satyajitsingh@Satyajits-MacBook-Pro.local>
2025-08-10 18:19:44 +0100

    changes due to v23

--- next.config.js
+++ next.config.js
@@ -4,13 +4,12 @@ const baseConfig = {
 };
 
 const isDev = process.env.NODE_ENV !== "production";
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: isDev, // enable PWA only in production
-  // optional: cache control tweaks
-  runtimeCaching: undefined,
 });
 
 module.exports = withPWA(baseConfig);
